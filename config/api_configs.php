9974d9348af60fecda9bb4e4f1f246df1754d976
EmersonBraun <emersonfbraun@gmail.com>
2020-07-07 14:23:00 -0300

    fix: add config admin_tables

--- config/api_configs.php
+++ config/api_configs.php
@@ -2,4 +2,12 @@
 
 return [
   'maximun_strikes' => 3,
+  'admin_tables' => ['email', 'permission', 'phone', 'system', 'user'],
+  'required_filables' => [
+    'email' => ['email', 'user_id'],
+    'permission' => ['permission'],
+    'phone' => ['type_phone', 'number', 'user_id'],
+    'system' => ['system', 'ip'],
+    'user' => ['name', 'rg', 'password', 'cpf', 'worplace', 'system_id'],
+  ]
 ];
